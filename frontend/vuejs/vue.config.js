fd7a1acdf3bf21fce7ccc78a3fa97db15aab61f8
davidcoderistov <davidcoderistov@gmail.com>
2023-12-24 17:33:07 +0100

    Configure webpack to enable hot module replacement

---
+++ frontend/vuejs/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    configureWebpack: {
+        entry: "./src/main.js",
+        devServer: {
+            hot: true,
+        },
+        watch: true,
+        watchOptions: {
+            ignored: /node_modules/,
+            poll: 1000,
+        },
+    },
+    transpileDependencies: true,
+};
