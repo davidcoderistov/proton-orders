96c95893e2360a6874270752e3dc984e7ec46932
davidcoderistov <davidcoderistov@gmail.com>
2023-12-25 21:08:47 +0100

    Refactor code

--- backend/flask/server.py
+++ backend/flask/server.py
@@ -14,19 +14,22 @@ CORS(app)
 
 client = MongoClient('mongo:27017')
 db = client['proton_db']
-proton_sales = db['proton_sales']
+proton_orders = db['proton_orders']
 
 
 def seed_db():
     try:
         if 'proton_db' not in client.list_database_names():
             df = read_csv('homework_order_lines.csv', index_col=0)
-            sales = df.to_dict(orient='records')
-            sales = [
-                {key.lower().replace(' ', '_'): datetime.strptime(value, '%Y-%m-%d') if key == 'Date' else value for key, value in sale.items()}
-                for sale in sales
+            orders = df.to_dict(orient='records')
+            orders = [
+                {
+                    key.lower().replace(' ', '_'): datetime.strptime(value, '%Y-%m-%d') if key == 'Date' else value
+                    for key, value in sale.items()
+                }
+                for sale in orders
             ]
-            proton_sales.insert_many(sales)
+            proton_orders.insert_many(orders)
             print("Database seeded successfully.")
     except ConnectionFailure as e:
         print(f"Failed to connect to MongoDB: {e}")
@@ -34,10 +37,10 @@ def seed_db():
         print(f"An error occurred during database seeding: {e}")
 
 
-@app.route('/api/sales')
-def get_sales():
-    sales = proton_sales.find().sort('Date', -1)
-    return encode_and_decode(list(sales))
+@app.route('/api/orders')
+def get_orders():
+    orders = proton_orders.find().sort('Date', -1)
+    return encode_and_decode(list(orders))
 
 
 if __name__ == "__main__":
