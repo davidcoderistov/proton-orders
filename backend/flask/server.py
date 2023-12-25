28750d97d59d53a771290f2916b940445af522b0
davidcoderistov <davidcoderistov@gmail.com>
2023-12-25 14:26:29 +0100

    Implement db seed from csv file

--- backend/flask/server.py
+++ backend/flask/server.py
@@ -3,20 +3,31 @@ import os
 
 from flask import Flask
 from pymongo import MongoClient
+from pandas import read_csv
+from datetime import datetime
+from pymongo.errors import ConnectionFailure
 
 app = Flask(__name__)
 
-client = MongoClient("mongo:27017")
+client = MongoClient('mongo:27017')
+db = client['proton_db']
+proton_sales = db['proton_sales']
 
-@app.route('/')
-def todo():
+
+def seed_db():
     try:
-        client.admin.command('ismaster')
-    except:
-        return "Server not available"
-    return "Hello from the MongoDB client!\n"
+        if 'proton_db' not in client.list_database_names():
+            df = read_csv('homework_order_lines.csv', index_col=0)
+            sales = df.to_dict(orient='records')
+            sales = [{**item, 'Date': datetime.strptime(item['Date'], '%Y-%m-%d')} for item in sales]
+            proton_sales.insert_many(sales)
+            print("Database seeded successfully.")
+    except ConnectionFailure as e:
+        print(f"Failed to connect to MongoDB: {e}")
+    except Exception as e:
+        print(f"An error occurred during database seeding: {e}")
 
 
 if __name__ == "__main__":
+    seed_db()
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
-
