980f5d1f1180894b32ce5e20db8d02ccdc19151a
davidcoderistov <davidcoderistov@gmail.com>
2023-12-26 19:18:44 +0100

    Create OrderPrice component

---
+++ frontend/vuejs/src/components/OrderPrice.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list-item-action-text class="text--primary order-price">
+    ${{ price | round }}
+  </v-list-item-action-text>
+</template>
+
+<script>
+export default {
+  name: 'OrderPrice',
+  props: {
+    price: {
+      type: Number,
+      default: 0,
+    }
+  },
+  filters: {
+    round(value) {
+      return Number(value).toFixed(2)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.order-price {
+  font-size: 16px;
+}
+</style>
