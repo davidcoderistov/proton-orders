43dff3daf0ad37cd53e990c52999ad0f819b11cc
davidcoderistov <davidcoderistov@gmail.com>
2023-12-26 19:19:51 +0100

    Create OrderQuantityAvatar component

---
+++ frontend/vuejs/src/components/OrderQuantityAvatar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-list-item-avatar color="pink" size="44">
+    <span class="white--text">{{ quantity }}x</span>
+  </v-list-item-avatar>
+</template>
+
+<script>
+export default {
+  name: 'OrderQuantityAvatar',
+  props: {
+    quantity: {
+      type: Number,
+      default: 1,
+    }
+  }
+}
+</script>
