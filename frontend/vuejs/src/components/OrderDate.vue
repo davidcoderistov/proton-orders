59cde1465037522d145832018f76bdb10832eea1
davidcoderistov <davidcoderistov@gmail.com>
2023-12-26 19:18:08 +0100

    Create OrderDate component

---
+++ frontend/vuejs/src/components/OrderDate.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-list-item-action-text :class="{ 'order-date': !dense }">
+    {{ timestamp | date }}
+  </v-list-item-action-text>
+</template>
+
+<script>
+export default {
+  name: 'OrderDate',
+  props: {
+    dense: {
+      type: Boolean,
+      default: false,
+    },
+    timestamp: {
+      type: Number,
+      default: 0,
+    }
+  },
+  filters: {
+    date(value) {
+      let date = new Date()
+      if (value) {
+        date = new Date(value * 1000)
+      }
+      const day = date.getDate()
+      const month = date.toLocaleString('default', {month: 'short'})
+      const year = date.getFullYear()
+
+      return `${day} ${month} ${year}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+.order-date {
+  padding-top: 2px;
+}
+</style>
