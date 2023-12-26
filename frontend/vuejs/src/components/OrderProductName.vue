256897f257e2363d4f271d87d40a6325b1076e59
davidcoderistov <davidcoderistov@gmail.com>
2023-12-26 19:19:14 +0100

    Create OrderProductName component

---
+++ frontend/vuejs/src/components/OrderProductName.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-list-item-title class="order-product-name">
+    {{ productName }}
+  </v-list-item-title>
+</template>
+
+<script>
+export default {
+  name: 'OrderProductName',
+  props: {
+    productName: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.order-product-name {
+  padding-bottom: 2px;
+}
+</style>
