dd6cb6e2a22e8577d6a9e74db94763f3f189c075
davidcoderistov <davidcoderistov@gmail.com>
2023-12-26 19:17:16 +0100

    Create OrderCustomerName component

---
+++ frontend/vuejs/src/components/OrderCustomerName.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-list-item-subtitle class="text--primary">
+    {{ customerName }}
+  </v-list-item-subtitle>
+</template>
+
+<script>
+export default {
+  name: 'OrderCustomerName',
+  props: {
+    customerName: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
