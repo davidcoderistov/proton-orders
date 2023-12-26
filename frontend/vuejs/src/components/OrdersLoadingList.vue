825a5b37442c72b8749adabf84a60adbe7afa41c
davidcoderistov <davidcoderistov@gmail.com>
2023-12-26 21:26:37 +0100

    Create OrdersLoadingList component

---
+++ frontend/vuejs/src/components/OrdersLoadingList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="index in items" :key="index">
+      <v-skeleton-loader type="list-item-avatar-two-line"/>
+      <v-divider v-if="index < items.length - 1"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OrdersLoadingList',
+  props: {
+    size: {
+      type: Number,
+      default: 0,
+    }
+  },
+  computed: {
+    items() {
+      return [...Array(this.size).keys()]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
