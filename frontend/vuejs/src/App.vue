9f475a40dd21a566f52671102b6fa8506cfed304
davidcoderistov <davidcoderistov@gmail.com>
2023-12-27 14:38:35 +0100

    Set up add orders UI

--- frontend/vuejs/src/App.vue
+++ frontend/vuejs/src/App.vue
@@ -1,60 +1,117 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <div class="orders-container">
+        <div class="orders-wrapper">
+          <v-card :width="450">
+            <v-toolbar
+                color="pink"
+                dark
+            >
+              <v-toolbar-title>Orders</v-toolbar-title>
+            </v-toolbar>
+            <orders-loading-list
+                v-if="ordersLoading"
+                :size="7"
+            />
+            <div v-else style="overflow-y: auto; max-height: 75vh;">
+              <orders-list
+                  v-model="selectedOrder"
+                  :orders="orders"
+              />
+            </div>
+          </v-card>
+          <v-btn
+              color="pink"
+              class="align-self-center white--text"
+              :disabled="ordersLoading"
+              @click="onAddOrder"
+          >
+            Add Order
+          </v-btn>
+          <v-card :width="450">
+            <v-toolbar
+                color="pink"
+                dark
+            >
+              <v-toolbar-title>Follow up Orders</v-toolbar-title>
+            </v-toolbar>
+            <orders-loading-list
+                v-if="ordersLoading"
+                :size="7"
+            />
+            <div v-else style="overflow-y: auto; max-height: 75vh;">
+              <follow-up-orders-list :orders="followUpOrders"/>
+            </div>
+          </v-card>
+        </div>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import OrdersLoadingList from "@/components/OrdersLoadingList.vue";
+import OrdersList from "@/components/OrdersList.vue";
+import FollowUpOrdersList from "@/components/FollowUpOrdersList.vue";
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
+  components: {OrdersLoadingList, OrdersList, FollowUpOrdersList},
   data: () => ({
-    //
+    orders: [],
+    ordersLoading: true,
+    followUpOrders: [],
+    selectedOrder: null,
   }),
+  methods: {
+    fetchOrders() {
+      this.ordersLoading = true
+      fetch('http://localhost:80/api/orders')
+          .then(response => response.json())
+          .then(data => {
+            if (Array.isArray(data)) {
+              this.orders = Array.from(data)
+            }
+          })
+          .catch(console.error)
+          .finally(() => {
+            this.ordersLoading = false
+          })
+    },
+    onAddOrder() {
+      const {selectedOrder, orders, followUpOrders} = this
+      if (!isNaN(selectedOrder) && typeof selectedOrder === 'number' && selectedOrder >= 0 && selectedOrder < orders.length) {
+        const order = {...orders[selectedOrder]}
+        const newOrders = [...orders]
+        const newFollowUpOrders = [...followUpOrders]
+
+        newOrders.splice(selectedOrder, 1)
+        newFollowUpOrders.push(order)
+
+        this.orders = newOrders
+        this.followUpOrders = newFollowUpOrders
+      }
+    }
+  },
+  created() {
+    this.fetchOrders()
+  }
 };
 </script>
+
+<style scoped>
+.orders-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.orders-wrapper {
+  display: flex;
+  flex-direction: row;
+  column-gap: 90px;
+}
+</style>
